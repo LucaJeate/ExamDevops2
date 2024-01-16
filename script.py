f37d2875bec5d6bcac2f696e29576d258973cec1
Lucas Legaz <lucas.legaz@gmail.com>
2024-01-16 10:57:36 +0100

    Push

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
 import os
+from numpy import log
 
 a = float(os.environ.get("A"))
 b = float(os.environ.get("B"))
 
-print (a * b)
+print ("log a * b")
+print(log(a * b))
+print ("log a + log b")
+print(log(a) + log(b))
